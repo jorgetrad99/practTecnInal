857093e073433689bfaf58dff5b371a717f11fee
Jorge Trad <jetradl@gmail.com>
2022-02-17 10:26:36 -0600

    Showing devices available list

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,20 +1,29 @@
-import { IonAlert, IonButton, IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { IonAlert, IonButton, IonCol, IonContent, IonGrid, IonHeader, IonItem, IonLabel, IonList, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import { useEffect, useState } from 'react';
 import { BluetoothSerial } from '@awesome-cordova-plugins/bluetooth-serial';
 
 import './Tab2.css';
 
+/* interface Device {
+  name: string,
+  address: string
+} */
+
 const Tab2: React.FC = () => {
+  const bluetoothSerial = BluetoothSerial;
+
   const [ aButtonState, setAButtonState ] = useState(false);
   const [ bButtonState, setBButtonState ] = useState(false);
   const [ cButtonState, setCButtonState ] = useState(false);
   const [ dButtonState, setDButtonState ] = useState(false);
 
+  const [ devices, setDevices ] = useState<any[]>([]);
+
   const [showAlert2, setShowAlert2] = useState(false);
   const [ message, setMessage ]  = useState('');
 
   const connectBluetooth = () => {
-    const bluetoothSerial = BluetoothSerial;
+    
     /* bluetoothSerial.connect('98:DA:20:00:E8:C2'); */
     
 
@@ -26,9 +35,20 @@ const Tab2: React.FC = () => {
     
   };
 
+  const listDevices = () => {
+    bluetoothSerial.list().then((response) => {
+      if(response.length > 0) {
+        setMessage('Some devices found');
+        setDevices(response);
+      }
+    }, (error) => {
+      setMessage(error);
+    })
+  }
+
   useEffect(() => {
     
-  }, [ message ]);
+  }, [ message, devices ]);
  
   return (
     
@@ -40,34 +60,55 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        { message.length > 0 && <IonAlert
-          isOpen={true}
-          onDidDismiss={() => {
-              setMessage('');
-              setShowAlert2(false)
+        { message.length > 0 && (
+            <IonAlert
+              isOpen={true}
+              onDidDismiss={() => {
+                  setMessage('');
+                  setShowAlert2(false)
 
-            }
-          }
-          cssClass='my-custom-class'
-          header={'Alert'}
-          subHeader={'Subtitle'}
-          message={message}
-          buttons={['Ok']}
-        /> }
+                }
+              }
+              cssClass='my-custom-class'
+              header={'Alert'}
+              /* subHeader={'Subtitle'} */
+              message={message}
+              buttons={['Ok']}
+            /> 
+          )
+        }
+        <IonButton onClick={connectBluetooth}>Is Bluetooth on?</IonButton>
+        <IonButton onClick={listDevices}>Devices List</IonButton>
+        {devices.length > 0 && (
+          <div>
+            <h3>Devices list</h3>
+            <IonList>
+            {devices.map((device) => {
+              return (
+                <IonItem>
+                  <IonLabel>{device.name}</IonLabel>
+                  <IonLabel>{device.address}</IonLabel>
+                </IonItem>
+              )
+            })}
+            </IonList>
+          </div>
+        )}
+        
         <div className='buttons'>
         <IonGrid>
           <IonRow>
             <IonCol>
-            <IonButton shape="round" size="large">A</IonButton>
+            <IonButton shape="round" size="large" className='controllers'>A</IonButton>
             </IonCol>
             <IonCol>
-              <IonButton shape="round" size="large">B</IonButton>
+              <IonButton shape="round" size="large" className='controllers'>B</IonButton>
             </IonCol>
             <IonCol>
-              <IonButton onClick={connectBluetooth} shape="round" size="large">C</IonButton>
+              <IonButton shape="round" size="large" className='controllers'>C</IonButton>
             </IonCol>
             <IonCol>
-              <IonButton shape="round" size="large">D</IonButton>
+              <IonButton shape="round" size="large" className='controllers'>D</IonButton>
             </IonCol>
           </IonRow>
         </IonGrid>
